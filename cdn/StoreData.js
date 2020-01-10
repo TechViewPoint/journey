fee83c055cc5a2006910ae24c14f815acd99d0e2
TechViewPoint <1502873704@qq.com>
2020-01-10 21:32:07 +0800

    add store data

---
+++ cdn/StoreData.js
@@ -0,0 +1,84 @@
+{
+    "categories": [
+      {
+        "name": "图书",
+        "key": "tab1"
+      },
+      {
+        "name": "居家装饰",
+        "key": "tab2"
+      },
+      {
+        "name": "科技产品",
+        "key": "tab3"
+      }
+    ],
+    "items": [
+      [
+        {
+          "imgUrl": "https://res.cloudinary.com/dm7h7e8xj/image/upload/v1559821647/theme6_qeeojf.jpg",
+          "title": "相框",
+          "price": 5.6,
+          "info": "这是一个好东西",
+          "itemImgs": [
+            "https://res.cloudinary.com/dm7h7e8xj/image/upload/v1559821647/theme6_qeeojf.jpg",
+            "https://res.cloudinary.com/dm7h7e8xj/image/upload/v1559821647/theme6_qeeojf.jpg"
+          ]
+        },
+        {
+          "imgUrl": "https://res.cloudinary.com/dm7h7e8xj/image/upload/v1559821647/theme6_qeeojf.jpg",
+          "title": "相框2",
+          "price": 15.6,
+          "info": "这是一个好东2西",
+          "itemImgs": [
+            "https://res.cloudinary.com/dm7h7e8xj/image/upload/v1559821647/theme6_qeeojf.jpg",
+            "https://res.cloudinary.com/dm7h7e8xj/image/upload/v1559821647/theme6_qeeojf.jpg"
+          ]
+        },
+        {
+          "imgUrl": "https://res.cloudinary.com/dm7h7e8xj/image/upload/v1559821647/theme6_qeeojf.jpg",
+          "title": "相框3",
+          "price": 150.6,
+          "info": "这是一个好东3西",
+          "itemImgs": [
+            "https://res.cloudinary.com/dm7h7e8xj/image/upload/v1559821647/theme6_qeeojf.jpg",
+            "https://res.cloudinary.com/dm7h7e8xj/image/upload/v1559821647/theme6_qeeojf.jpg"
+          ]
+        }
+      ],
+      [
+        {
+          "imgUrl": "https://res.cloudinary.com/dm7h7e8xj/image/upload/v1559821647/theme6_qeeojf.jpg",
+          "title": "相框",
+          "price": 5.6,
+          "info": "这是一个好东西",
+          "itemImgs": [
+            "https://res.cloudinary.com/dm7h7e8xj/image/upload/v1559821647/theme6_qeeojf.jpg",
+            "https://res.cloudinary.com/dm7h7e8xj/image/upload/v1559821647/theme6_qeeojf.jpg"
+          ]
+        },
+        {
+          "imgUrl": "https://res.cloudinary.com/dm7h7e8xj/image/upload/v1559821647/theme6_qeeojf.jpg",
+          "title": "手机",
+          "price": 5.6,
+          "info": "这是一个好东西",
+          "itemImgs": [
+            "https://res.cloudinary.com/dm7h7e8xj/image/upload/v1559821647/theme6_qeeojf.jpg",
+            "https://res.cloudinary.com/dm7h7e8xj/image/upload/v1559821647/theme6_qeeojf.jpg"
+          ]
+        }
+      ],
+      [
+        {
+          "imgUrl": "https://res.cloudinary.com/dm7h7e8xj/image/upload/v1559821647/theme6_qeeojf.jpg",
+          "title": "相框",
+          "price": 5.6,
+          "info": "这是一个好东西",
+          "itemImgs": [
+            "https://res.cloudinary.com/dm7h7e8xj/image/upload/v1559821647/theme6_qeeojf.jpg",
+            "https://res.cloudinary.com/dm7h7e8xj/image/upload/v1559821647/theme6_qeeojf.jpg"
+          ]
+        }
+        ]
+    ]
+  }
